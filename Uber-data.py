62959a36b2e38a5e661af53a7087ae6ada97fc7d
Antoine Dupont <tony_dupont@yahoo.com>
2024-09-19 13:51:30 +0200

    adding features for uber data

--- Uber-data.py
+++ Uber-data.py
@@ -19,6 +19,18 @@ data2 = load_data()
 # Title
 st.title("🚗 Uber Data Analysis")
 
+show_pickup = st.checkbox("Show Pickup Data", value=True)
+show_dropoff = st.checkbox("Show Dropoff Data", value=False)
+
+filtered_data_pickup = pd.DataFrame()
+filtered_data_dropoff = pd.DataFrame()
+
+if show_pickup:
+    filtered_data_pickup = data2[['tpep_pickup_datetime', 'pickup_latitude', 'pickup_longitude']]
+
+if show_dropoff:
+    filtered_data_dropoff = data2[['tpep_pickup_datetime', 'dropoff_latitude', 'dropoff_longitude']]
+
 # Add a new column for tip/fare ratio
 data2['tip/fare'] = data2['tip_amount'] / data2['fare_amount']
 
@@ -28,13 +40,33 @@ data2['tip/fare'].replace(to_replace=0, value=np.nan, inplace=True)
 data2['tpep_pickup_datetime'] = pd.to_datetime(data2['tpep_pickup_datetime'])
 data2['tpep_dropoff_datetime'] = pd.to_datetime(data2['tpep_dropoff_datetime'])
 
-st.title("Uber Pickups in New York City (2014)")
 
 
-### Visualization 2: Bar Chart of Pickups by Hour ###
-st.subheader(f"1. Pickups over time (All Data)")
-hourly_pickups_filtered = data2['tpep_pickup_datetime'].dt.hour.value_counts().sort_index()
-st.bar_chart(hourly_pickups_filtered)
+if show_pickup and show_dropoff:
+    st.write("### Showing both Pickup and Dropoff Data")
+    filtered_data_pickup.columns = ['tpep_pickup_datetime', 'latitude', 'longitude']
+    filtered_data_dropoff.columns = ['tpep_pickup_datetime', 'latitude', 'longitude']
+    filtered_data = pd.concat([filtered_data_pickup, filtered_data_dropoff])
+elif show_pickup:
+    st.title("Uber Pickups in New York City (2014)")
+    filtered_data = filtered_data_pickup.rename(columns={'pickup_latitude': 'latitude', 'pickup_longitude': 'longitude'})
+
+    ### Visualization 1: Bar Chart of Pickups by Hour ###
+    st.subheader(f"1. Pickups over time (All Data)")
+    hourly_pickups_filtered = data2['tpep_pickup_datetime'].dt.hour.value_counts().sort_index()
+    st.bar_chart(hourly_pickups_filtered, color = '#23395B')
+
+
+elif show_dropoff:
+    st.title("Uber Dropoffs in New York City (2014)")
+    filtered_data = filtered_data_dropoff.rename(columns={'dropoff_latitude': 'latitude', 'dropoff_longitude': 'longitude'})
+
+    ### Visualization 1: Bar Chart of Pickups by Hour ###
+    st.subheader(f"1. Pickups over time (All Data)")
+    hourly_dropoffs_filtered = data2['tpep_dropoff_datetime'].dt.hour.value_counts().sort_index()
+    st.bar_chart(hourly_dropoffs_filtered, color='#CBF7ED')
+
+
 
 # Data per hour
 st.write("### Filter the data per hour ###")
@@ -48,49 +80,3 @@ pickup_counts_by_minute = filtered_data.set_index('tpep_pickup_datetime').resamp
 st.line_chart(pickup_counts_by_minute)
 
 
-### Visualization 2: Map of Uber Pickups ###
-st.subheader("3. Geographic Distribution of Uber Pickups (Map)")
-
-# Use pydeck for interactive map visualization
-st.map(filtered_data[['Lat', 'Lon']].dropna())
-
-# Advanced Map using pydeck
-st.subheader("Advanced Pickup Map")
-map_layer = pdk.Layer(
-    "HexagonLayer",
-    data=filtered_data[['Lat', 'Lon']],
-    get_position='[Lon, Lat]',
-    radius=100,
-    elevation_scale=4,
-    elevation_range=[0, 1000],
-    pickable=True,
-    extruded=True,
-)
-
-# Set the map view
-view_state = pdk.ViewState(
-    longitude=-74.00, latitude=40.71, zoom=10, pitch=50,
-)
-
-# Render the map
-map_view = pdk.Deck(layers=[map_layer], initial_view_state=view_state)
-st.pydeck_chart(map_view)
-
-### Visualization 4: Heatmap of Pickup Density ###
-st.subheader("4. Heatmap of Uber Pickup Density")
-
-# Create a heatmap using Seaborn
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.histplot(data=filtered_data, x="Lon", y="Lat", bins=30, pthresh=.1, cmap="coolwarm", ax=ax)
-plt.title("Heatmap of Uber Pickups")
-st.pyplot(fig)
-
-### Visualization 5: Histogram of Pickups per Day ###
-st.subheader("5. Histogram of Pickups per Day")
-
-# Group by day and plot the distribution
-data2['day'] = data2['tpep_tpep_pickup_datetime'].dt.day
-fig, ax = plt.subplots()
-sns.histplot(data2['day'], kde=False, ax=ax)
-ax.set_title("Pickups per Day")
-st.pyplot(fig)
